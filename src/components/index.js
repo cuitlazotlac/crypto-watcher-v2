347c43b47fbc25ccae65da73a1c4510aa77be8e7
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-12-05 11:07:24 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Homepage } from './Homepage';
+export { default as Navbar } from './shared/Navbar';
+export { default as News } from './News';
+export { default as Cryptocurrencies } from './Cryptos';
+export { default as Exchanges } from './Exchanges';
+export { default as CryptoDetails } from './CryptoDetails';
